2c01f71d0c9173db3f79b9b68f110e9f0fbf9f67
MaxymKlochko <klo4komax@gmail.com>
2022-01-19 19:40:49 +0300

    hw3

--- src/App.js
+++ src/App.js
@@ -2,7 +2,6 @@ import {Route, Routes} from "react-router-dom";
 
 import Layout from "./components/UsersComponent/Layout/Layout";
 import Users from "./pages/UsersPages/Users/Users";
-import Posts from "./pages/UsersPages/Posts/Posts";
 import Albums from "./pages/UsersPages/Albums/Albums";
 import Photos from "./pages/UsersPages/Photos/Photos";
 
@@ -17,9 +16,6 @@ function App() {
                     <Route path={':albums'} element={<Albums/>}>
                         <Route path={':photos'} element={<Photos/>}/>
                     </Route>
-                </Route>
-                <Route path={'posts'} element={<Posts/>}>
-
                 </Route>
               </Route>
           </Routes>
--- src/components/UsersComponent/Layout/Layout.js
+++ src/components/UsersComponent/Layout/Layout.js
@@ -8,7 +8,6 @@ const Layout = () => {
         <div>
             <div className={css.header}>
                 <h2><NavLink to={'/users'}>Users</NavLink></h2>
-                <h2><NavLink to={'/posts'}>Posts</NavLink></h2>
             </div>
 
 
