0e04b78bf2d6c9a6a91d339bf2096e0b3d326f8a
MaxymKlochko <klo4komax@gmail.com>
2022-01-27 15:41:59 +0300

    hw4

--- src/App.js
+++ src/App.js
@@ -1,23 +1,69 @@
-import logo from './logo.svg';
 import './App.css';
+import {useReducer} from "react";
+
+ let reducer = (state, action) => {
+
+     switch (action.type){
+         case 'inc':
+             return {...state, count1: state.count1 + 1}
+         case 'dec':
+             return {...state, count1: state.count1 - 1}
+         case 'reset':
+             return {...state, count1: state.count1 = 0}
+     }
+
+     switch (action.type){
+         case 'increase':
+             return {...state, count2: state.count2 + 10}
+         case 'decrease':
+             return {...state, count2: state.count2 - 10}
+         case 'initial':
+             return {...state, count2: state.count2 = 10}
+     }
+
+     switch (action.type){
+         case 'inc_100':
+             return {...state, count3: state.count3 + 100}
+         case 'dec_100':
+             return {...state, count3: state.count3 - 100}
+         case 'ini_100':
+             return {...state, count3: state.count3 = 100}
+     }
+
+
+
+   return state
+ }
+
 
 function App() {
+
+  let [state, dispatch] = useReducer(reducer, {count1: 0, count2:10, count3:100});
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+        <div>
+            <div>{state.count1}</div>
+            <button onClick={()=> dispatch({type:'inc'})}>increment</button>
+            <button onClick={()=> dispatch({type:'dec'})}>decrement</button>
+            <button onClick={()=> dispatch({type:'reset'})}>reset</button>
+        </div>
+
+        <div>
+            <div>{state.count2}</div>
+            <button onClick={()=> dispatch({type:'increase'})}>increase by 10</button>
+            <button onClick={()=> dispatch({type:'decrease'})}>decrease by 10</button>
+            <button onClick={()=> dispatch({type:'initial'})}>initial</button>
+        </div>
+
+        <div>
+            <div>{state.count3}</div>
+            <button onClick={()=> dispatch({type:'inc_100'})}>increase by 100</button>
+            <button onClick={()=> dispatch({type:'dec_100'})}>decrement by 100</button>
+            <button onClick={()=> dispatch({type:'ini_100'})}>initial</button>
+        </div>
+
     </div>
   );
 }
