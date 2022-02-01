63ec1ed054b6a350cbe34c05243e02cb422e7b23
MaxymKlochko <klo4komax@gmail.com>
2022-02-01 14:42:18 +0300

    hw5 cars with validations

--- src/components/Form/Form.js
+++ src/components/Form/Form.js
@@ -2,12 +2,16 @@ import React from 'react';
 import {useForm} from "react-hook-form";
 import {useDispatch} from "react-redux";
 
-
 import {addCar} from "../../store";
+import css from "./Form.module.css"
 
 const Form = () => {
 
-    let {handleSubmit,register,reset} = useForm();
+    let { handleSubmit,
+          register,
+          reset,
+          formState:{errors}
+        } = useForm({mode:"onBlur"});
 
     let dispatch = useDispatch();
 
@@ -17,12 +21,50 @@ const Form = () => {
     }
 
     return (
-        <div>
+        <div className={css.form}>
+            <h1>Add a new car<hr/></h1>
             <form onSubmit={handleSubmit(submit)}>
-                <label>Brand: <input type="text" {...register('brand')}/> </label>
-                <label>Price: <input type="text" {...register('price')}/> </label>
-                <label>Year: <input type="text" {...register('year')}/> </label>
-                <button>Save a car</button>
+
+                <label>Brand: <input
+                    type="text"
+                    {...register('brand',{
+                        required: 'Field is required!'
+                    })}/>
+                </label>
+                <div>
+                    {errors?.brand && <p>{errors?.brand?.message || "Error"}</p>}
+                </div>
+
+                <label>Price: <input
+                    type="text"
+                    {...register('price',{
+                        required: 'Field is required!',
+                    })}/>
+                </label>
+                <div>
+                    {errors?.price && <p>{errors?.price?.message || "Error"}</p>}
+                </div>
+
+                <label>Year:  <input
+                    type="text"
+                    {...register('year',{
+                        required:true,
+                        maxLength: {
+                            value:4,
+                            message:'Maximum 4 digits!'
+                        },
+                        minLength: {
+                            value:4,
+                            message:'Minimum 4 digits'
+                        }
+                    })}/>
+                </label>
+                <div>
+                    {errors?.year && <p>{errors?.year?.message || 'Field is required!' }</p>}
+                </div>
+
+                <input type="submit"/>
+
             </form>
         </div>
     );
